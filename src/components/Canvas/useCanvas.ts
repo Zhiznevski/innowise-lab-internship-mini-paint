78213e8a755dd41de4f873e043590bae6c8436bb
Zhiznevski <w.heisenberggg@gmail.com>
2024-04-17 22:47:39 +0300

    refactor: refactor types and contants in useCanvas

--- src/components/Canvas/useCanvas.ts
+++ src/components/Canvas/useCanvas.ts
@@ -1,13 +1,22 @@
 import { useEffect, useRef, useState } from 'react';
 
-export interface Coordinates {
+interface Coordinates {
   startX: number;
   startY: number;
 }
 
-type CurrentToolsType = 'brush' | 'line' | 'rect' | 'circle';
+enum Tools {
+  brush = 'brush',
+  line = 'line',
+  rect = 'rect',
+  circle = 'circle',
+}
 
-function useCanvas(lineColor = 'black', lineWidth = 5, currentTool: CurrentToolsType = 'line') {
+type ToolType = keyof typeof Tools;
+
+const LEFT_MOUSE_BUTTON_NUMBER = 1;
+
+function useCanvas(lineColor = 'black', lineWidth = 5, currentTool: ToolType = 'brush') {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const contextRef = useRef<CanvasRenderingContext2D | null>(null);
   const [isMouseDown, setIsMouseDown] = useState(false);
@@ -21,11 +30,13 @@ function useCanvas(lineColor = 'black', lineWidth = 5, currentTool: CurrentTools
   };
 
   const mouseDownHandler = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    if (e.buttons !== LEFT_MOUSE_BUTTON_NUMBER) return;
     const target = e.target as HTMLCanvasElement;
-    if (e.buttons !== 1) return;
+    const startX = e.pageX - target.offsetLeft;
+    const startY = e.pageY - target.offsetTop;
     contextRef.current?.beginPath();
-    setStartCoords({ startX: e.pageX - target.offsetLeft, startY: e.pageY - target.offsetTop });
-    contextRef.current?.moveTo(e.pageX - target.offsetLeft, e.pageY - target.offsetTop);
+    contextRef.current?.moveTo(startX, startY);
+    setStartCoords({ startX: startX, startY: startY });
     setSnapshot(canvasRef.current?.toDataURL());
     setIsMouseDown(true);
   };
@@ -35,10 +46,12 @@ function useCanvas(lineColor = 'black', lineWidth = 5, currentTool: CurrentTools
   };
 
   const mouseMoveHandler = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    const target = e.target as HTMLCanvasElement;
+    const currentX = e.pageX - target.offsetLeft;
+    const currentY = e.pageY - target.offsetTop;
     if (isMouseDown) {
-      const target = e.target as HTMLCanvasElement;
-      if (currentTool === 'brush') {
-        contextRef.current?.lineTo(e.pageX - target.offsetLeft, e.pageY - target.offsetTop);
+      if (currentTool === Tools.brush) {
+        contextRef.current?.lineTo(currentX, currentY);
         contextRef.current?.stroke();
       } else {
         const img = new Image();
@@ -61,24 +74,23 @@ function useCanvas(lineColor = 'black', lineWidth = 5, currentTool: CurrentTools
               );
               contextRef.current?.beginPath();
 
-              if (currentTool === 'line') {
+              if (currentTool === Tools.line) {
                 contextRef.current?.moveTo(startCoods.startX, startCoods.startY);
-                contextRef.current?.lineTo(e.pageX - target.offsetLeft, e.pageY - target.offsetTop);
+                contextRef.current?.lineTo(currentX, currentY);
               }
 
-              if (currentTool === 'rect') {
+              if (currentTool === Tools.rect) {
                 contextRef.current?.rect(
                   startCoods.startX,
                   startCoods.startY,
-                  e.pageX - target.offsetLeft - startCoods.startX,
-                  e.pageY - target.offsetTop - startCoods.startY
+                  currentX - startCoods.startX,
+                  currentY - startCoods.startY
                 );
               }
 
-              if (currentTool === 'circle') {
+              if (currentTool === Tools.circle) {
                 const radius = Math.sqrt(
-                  (e.pageX - target.offsetLeft - startCoods.startX) ** 2 +
-                    (e.pageY - target.offsetTop - startCoods.startY) ** 2
+                  (currentX - startCoods.startX) ** 2 + (currentY - startCoods.startY) ** 2
                 );
                 contextRef.current?.arc(
                   startCoods.startX,
