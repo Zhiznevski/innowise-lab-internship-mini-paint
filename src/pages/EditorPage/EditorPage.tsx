25f1367b8eaf4b755dc22b7f80e0af55260a84f6
Zhiznevski <w.heisenberggg@gmail.com>
2024-04-25 00:19:54 +0300

    feat: add redirect from editor page for unauthorized users

--- src/pages/EditorPage/EditorPage.tsx
+++ src/pages/EditorPage/EditorPage.tsx
@@ -3,9 +3,20 @@ import { auth } from '../../api/config';
 import Spinner from '../../ui/Spinner/Spinner';
 import { Canvas } from '../../modules/Canvas';
 import { ToolPanels } from '../../modules/ToolsPanel';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { LOGIN_ROUTE } from '../../constants/routes';
 
 function EditorPage() {
   const [user, loading] = useAuthState(auth);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate(LOGIN_ROUTE);
+    }
+  }, [user, navigate]);
 
   if (loading) {
     return <Spinner variant="circle" />;
